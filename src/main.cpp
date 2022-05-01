7995a3ee8db73305089d95eca34971f404b62fa1
MattHandzel <matthewwalkosz@gmail.com>
2022-04-30 22:23:01 -0500

    Added the ability to control the speed of the main loop itself as well as learned how to read data from a potentiometer

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <stdio.h>
 
-#define PHI PI * 2
+#define TAU PI * 2
 
 class VibrationMotor
 {
 private:
   // Creates varaibles for the vibration motors position (in rads),
   // the length of the "arc" it is going to span
-  float m_position, m_arcLength = PHI;
+  float m_position, m_arcLength = TAU;
   int m_pin;
 
 public:
@@ -55,11 +55,11 @@ public:
     // stays between 0 and 2pi
     while (angle > (PI + this->m_position))
     {
-      angle -= PHI;
+      angle -= TAU;
     }
     while (angle < (-PI + this->m_position))
     {
-      angle += PHI;
+      angle += TAU;
     }
 
     float power = -square((angle - this->m_position) / (m_arcLength / 2)) + 1;
@@ -77,7 +77,7 @@ class Bracelet
 
   int AVALIABLE_PINS[4] = {A0, A1, A2, A3};
 
-  float m_kARCLENGTH = PHI / 4;
+  float m_kARCLENGTH = TAU / 4;
 
 public:
   Bracelet()
@@ -88,7 +88,7 @@ public:
   {
     for (int i = 0; i < this->m_nMotors; i++)
     {
-      this->m_motors[i] = new VibrationMotor(this->AVALIABLE_PINS[i], (PHI / m_nMotors) * i, this->m_kARCLENGTH);
+      this->m_motors[i] = new VibrationMotor(this->AVALIABLE_PINS[i], (TAU / m_nMotors) * i, this->m_kARCLENGTH);
     }
   }
 
@@ -107,14 +107,30 @@ void setup()
 {
   Serial.begin(9600);
   b.generateMotors();
-  pinMode(3, OUTPUT);
+  pinMode(A5, INPUT_PULLUP);
 }
 
-float currentAngle = 0;
+float currentAngle = 0, speed = 0;
+long startTime, endTime;
+int timePerLoop = 50;
 void loop()
 {
+  startTime = millis();
   b.setAngle(currentAngle);
-  currentAngle += PI / 16;
-  delay(100);
-  Serial.println(currentAngle);
+
+  speed = (min((float)analogRead(A5) / 220, 1));
+
+  currentAngle += PI / 2 * speed;
+
+  if (currentAngle > 6)
+  {
+    currentAngle -= TAU;
+  }
+
+  endTime = millis();
+  while (endTime - startTime < timePerLoop)
+  {
+    delay(1);
+    endTime = millis();
+  }
 }
