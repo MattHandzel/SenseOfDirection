066ee747852d1d07c4a0d305da886093a8cf21b0
MattHandzel <matthewwalkosz@gmail.com>
2022-09-20 21:59:57 -0500

    Collected some data, made it so that the thing can detect north :D

--- src/main_01.cpp
+++ src/main_01.cpp
@@ -20,18 +20,21 @@ ADXL345 accel;
 
 int16_t ax, ay, az;
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 6)
+#define LED_PIN 6 // (Arduino is 13, Teensy is 6)
+#define DATA_COLLECTION_BUTTON 3
 bool blinkState = false;
 
 void setup()
 {
   // join I2C bus (I2Cdev library doesn't do this automatically)
   Wire.begin();
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(DATA_COLLECTION_BUTTON, INPUT_PULLUP);
 
   // initialize serial communication
   // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
   // it's really up to you depending on your project)
-  Serial.begin(14400);
+  Serial.begin(115200);
 
   // initialize device
   Serial.println("Initializing I2C devices...");
@@ -48,8 +51,17 @@ void setup()
 
   Serial.println("Testing device connections...");
   Serial.println(mag.testConnection() ? "HMC5883L connection successful" : "HMC5883L connection failed");
+  delay(6);
 }
 
+int verbose = 0;
+const bool printAccel = false;
+const bool printGyro = false;
+const bool printMag = true;
+const int maxDataCount = 100;
+int data[3][100];
+int printCounter = 0;
+int dataIndex = 0;
 void loop()
 {
   // read raw accel measurements from device
@@ -57,35 +69,82 @@ void loop()
   mag.getHeading(&mx, &my, &mz);
   gyro.getRotation(&gx, &gy, &gz);
 
+
+  // Experimental data shows that mz is bigger than expected
+  mz *= 0.63;
+  mz += 50; // Experimental offset
+
+
+  my += 37; // experimental offset
+
+  if(my < -270){
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("North");
+  }
+
   // Serial.print(az);
   //  display tab-separated accel x/y/z values
-  Serial.print("accelga ");
-  Serial.print(ax);
-  Serial.print(" ");
-  Serial.print(ay);
-  Serial.print(" ");
-  Serial.print(az);
-  Serial.print(" ");
+  if (verbose >= 1)
+  {
+    if (printAccel)
+    {
+      Serial.print("accel ");
+      Serial.print(ax);
+      Serial.print(" ");
+      Serial.print(ay);
+      Serial.print(" ");
+      Serial.print(az);
+      Serial.print(" ");
+    }
+    if (printMag)
+    {
+      Serial.print("mag ");
+      Serial.print(mx);
+      Serial.print(" ");
+      Serial.print(my);
+      Serial.print(" ");
+      Serial.println(mz);
+    }
+    if (printGyro)
+    {
+      // Serial.print("gyro ");
+      Serial.print(gx);
+      Serial.print(" ");
+      Serial.print(gy);
+      Serial.print(" ");
+      Serial.print(az);
+      Serial.print(" ");
+      Serial.println();
+    }
+  }
 
-  Serial.print("magenta ");
-  Serial.print(mx);
-  Serial.print(" ");
-  Serial.print(my);
-  Serial.print(" ");
-  Serial.print(mz);
-  Serial.print(" ");
+  if (Serial && printCounter < dataIndex) //(Serial)
+  {
+    printCounter++;
+    printCounter = printCounter > dataIndex ? dataIndex : printCounter;
+    Serial.print(data[0][printCounter]);
+    Serial.print(" ");
+    Serial.print(data[1][printCounter]);
+    Serial.print(" ");
+    Serial.println(data[2][printCounter]);
+  }
+  if (!digitalRead(DATA_COLLECTION_BUTTON))
+  {
+    if (dataIndex < maxDataCount)
+    {
+      data[0][dataIndex] = mx;
+      data[1][dataIndex] = my;
+      data[2][dataIndex] = mz;
+    }
+    else
+    {
+      digitalWrite(LED_PIN, HIGH);
+    }
+  }
 
-  Serial.print("gyroska ");
-  Serial.print(gx);
-  Serial.print(" ");
-  Serial.print(gy);
-  Serial.print(" ");
-  Serial.print(az);
-  Serial.print(" ");
-  Serial.println();
-  delay(10);
-
-  // blink LED to indicate activity
-  blinkState = !blinkState;
-  digitalWrite(LED_PIN, blinkState);
+  delay(1000 / 15);
 }
+
+// mag N -> -116; 497; -289
+// mag S -> -136; -432; 185
+// 220
